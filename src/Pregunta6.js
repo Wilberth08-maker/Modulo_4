a6da6621c4ce82abd29bf62b4c1900e9cafcdfa6
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-06-19 22:08:04 -0500

    6. En el evento onChange se utilizó setValue para actualizar correctamente el estado reactivo del input

---
+++ src/Pregunta6.js
@@ -0,0 +1,21 @@
+// ❓ Pregunta 6: Estado mutado directamente en onChange
+// 🧩 ¿Qué está mal aquí? ¿Por qué no funciona el input?
+// ¿Cómo arreglarías el código para que funcione correctamente?
+
+import { useState } from 'react';
+
+function InputTracker() {
+    const [value, setValue] = useState('');
+
+    return (
+        <input
+            type="text"
+            value={value}
+            onChange={(e) => {
+                setValue(e.target.value);
+            }}
+        />
+    );
+}
+
+export default InputTracker;
