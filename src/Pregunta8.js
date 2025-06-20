c6f0ab2619e9f14654f617bf6f82117c6814c811
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-06-19 22:27:04 -0500

    8. Uso incorrecto de async/await en useEffect para consumo de la API, se movió la función dentro de useEffect y se agrego la llamada correcta de la función dentro del efecto

---
+++ src/Pregunta8.js
@@ -0,0 +1,42 @@
+// ❓ Pregunta 8: Consumo de PokéAPI con async/await y errores
+// 🧩 ¿Qué error tiene el uso de useEffect aquí?
+// Corrige el código para que funcione correctamente y explique por qué.
+
+
+import { useState, useEffect } from 'react';
+
+function PokemonCard({ id }) {
+    const [pokemon, setPokemon] = useState(null);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchPokemon = async () => {        
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+                const data = await response.json();
+                setPokemon(data);
+            } catch {
+
+                setError(true);
+            }
+        };
+        
+    fetchPokemon();
+    }, [id]);
+
+    if (error) return <p>Algo salió mal...</p>;
+    if (!pokemon) return <p>Cargando...</p>;
+
+
+    return (
+        <div style={{ border: '2px solid #f00', padding: 20, borderRadius: 10 }}>
+            <h2>{pokemon.name.toUpperCase()}</h2>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} width={150} />
+            <p>Tipo: {pokemon.types[0].type.name}</p> {/* ERROR: ¿Y si no tiene tipo? */}
+            <p>Especie: {pokemon.species.name}</p> {/* ERROR: especies es un objeto anidado */}
+        </div>
+    );
+}
+
+export default PokemonCard;
